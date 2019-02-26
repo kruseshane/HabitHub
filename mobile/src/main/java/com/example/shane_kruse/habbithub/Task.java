7562bad097ca2de5ae9bc49be5c925352cbec571
disasterpiece9000 <35543856+disasterpiece9000@users.noreply.github.com>
2019-02-26 16:52:27 -0500

    Added DbHandeler class

--- mobile/src/main/java/com/example/shane_kruse/habbithub/Task.java
+++ mobile/src/main/java/com/example/shane_kruse/habbithub/Task.java
@@ -1,15 +1,27 @@
 package com.example.shane_kruse.habbithub;
 
+import java.util.Date;
+
 public class Task {
-    private String descr;
-    private int count;
-    private int current_count;
+    private String descr;       //Description of Task
+    private int goal;           //Number of times Task should be completed
+    private int prog;           //Current progress towards the goal
+    private Date due_date;      //Date/Time that the task must be completed by
+    private String icon;        //Icon ID
+    private boolean completed;  //Has the goal been met
+    private String interval;    //Daily, weekly, monthly, etc
+    private String color;       //Color hex
 
 
-    public Task(String descr, int count) {
+    public Task(String descr, int goal, int prog, Date due_date, String icon, boolean completed, String interval, String color) {
         this.descr = descr;
-        this.count = count;
-        this.current_count = 1;
+        this.goal = goal;
+        this.prog = prog;
+        this.due_date = due_date;
+        this.icon = icon;
+        this.completed = completed;
+        this.interval = interval;
+        this.color = color;
     }
 
     public String getDescr() {
@@ -20,19 +32,60 @@ public class Task {
         this.descr = descr;
     }
 
-    public int getCount() {
-        return count;
+    public int getGoal() {
+        return goal;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public void setGoal(int goal) {
+        this.goal = goal;
     }
 
-    public int getCurrent_count() {
-        return current_count;
+    public int getProg() {
+        return prog;
     }
 
-    public void setCurrent_count(int current_count) {
-        this.current_count = current_count;
+    public void setProg(int prog) {
+        this.prog = prog;
     }
+
+    public Date getDue_date() {
+        return due_date;
+    }
+
+    public void setDue_date(Date due_date) {
+        this.due_date = due_date;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public void setInterval(String interval) {
+        this.interval = interval;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
