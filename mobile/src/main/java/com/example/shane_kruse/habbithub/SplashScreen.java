bc48eae15b705b4de4ddf042130c252393cdd954
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-02-26 17:20:26 -0500

    Add splash screen

---
+++ mobile/src/main/java/com/example/shane_kruse/habbithub/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.shane_kruse.habbithub;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Timer timer = new Timer(3000, 3000);
+        timer.start();
+    }
+
+    private class Timer extends CountDownTimer {
+
+        public Timer(long millisLength, long millisUpdate) {
+            super(millisLength, millisUpdate);
+        }
+
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
